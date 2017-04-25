1a0b2070fca9a50f173dc6f96f1b8af2140ceac0
dimitri <dimi309@users.noreply.github.com>
2017-04-25 23:15:16 +0200

    Switched main tutorial to GLFW

--- main.cpp
+++ main.cpp
@@ -1,41 +1,69 @@
 #include <small3d/Renderer.hpp>
 #include <small3d/SceneObject.hpp>
+#include <GLFW/glfw3.h>
 
 using namespace small3d;
+
+bool down, left, right, up, esc, enter;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+    down = true;
+  if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+    up = true;
+  if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+    left = true;
+  if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+    right = true;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    esc = true;
+  if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE)
+    down = false;
+  if (key == GLFW_KEY_UP && action == GLFW_RELEASE)
+    up = false;
+  if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
+    left = false;
+  if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
+    right = false;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+    esc = false;
+
+}
 	
 int main(int argc, char **argv) {
 	
-	Renderer renderer("Ball demo");
+  Renderer renderer("Ball demo");
+
+  GLFWwindow* window = renderer.getWindow();
+
+  glfwSetKeyCallback(window, keyCallback);
 	
-	SceneObject ball("ball", "resources/ball.obj");
-	ball.colour = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
-	ball.offset = glm::vec3(0.0f, -1.0f, -8.0f);
+  SceneObject ball("ball", "resources/ball.obj");
+  ball.colour = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
+  ball.offset = glm::vec3(0.0f, -1.0f, -8.0f);
 	
-	while(true){
-        SDL_Event event;
-        if (SDL_PollEvent(&event)) {
-			if (event.key.keysym.sym == SDLK_ESCAPE) {
-				break;
-			}
-	  }
+  while (!glfwWindowShouldClose(window) && !esc) {
+
+    glfwPollEvents();
+    if (esc)
+      break;
+	
+    if (up)
+      ball.offset.z -= 0.1f;
+    else if (down)
+      ball.offset.z += 0.1f;
+    else if (left)
+      ball.offset.x -= 0.1f;
+    else if (right)
+      ball.offset.x += 0.1f;
 	  
-	const Uint8 *keyState = SDL_GetKeyboardState(NULL);
-	
-	if (keyState[SDL_SCANCODE_UP] == 1)
-		ball.offset.z -= 0.1f;
-	else if (keyState[SDL_SCANCODE_DOWN] == 1)
-		ball.offset.z += 0.1f;
-	else if (keyState[SDL_SCANCODE_LEFT] == 1)
-		ball.offset.x -= 0.1f;
-	else if (keyState[SDL_SCANCODE_RIGHT] == 1)
-		ball.offset.x += 0.1f;
-	  
-	renderer.clearScreen();
-	renderer.render(ball);
-	renderer.swapBuffers();
+    renderer.clearScreen();
+    renderer.render(ball);
+    renderer.swapBuffers();
 		
-	}
+  }
 	
-return 0;
+  return 0;
 
 }
