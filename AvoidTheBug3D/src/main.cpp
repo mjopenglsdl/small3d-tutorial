f3458365f8bcdb0e2c3b0b7f11aaff3d5e4088be
dimitri <dimi309@users.noreply.github.com>
2017-05-20 15:46:29 +0200

    Avoid the Bug: Removed some mingw code.

--- AvoidTheBug3D/src/main.cpp
+++ AvoidTheBug3D/src/main.cpp
@@ -31,67 +31,59 @@ KeyInput input;
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
-        input.down = true;
-    if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-        input.up = true;
-    if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
-        input.left = true;
-    if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-        input.right = true;
-    if (key == GLFW_KEY_ENTER && action == GLFW_PRESS)
-        input.enter = true;
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        input.esc = true;
-    if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE)
-        input.down = false;
-    if (key == GLFW_KEY_UP && action == GLFW_RELEASE)
-        input.up = false;
-    if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
-        input.left = false;
-    if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
-        input.right = false;
-    if (key == GLFW_KEY_ENTER && action == GLFW_RELEASE)
-        input.enter = false;
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-        input.esc = false;
-   
+  if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+    input.down = true;
+  if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+    input.up = true;
+  if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+    input.left = true;
+  if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+    input.right = true;
+  if (key == GLFW_KEY_ENTER && action == GLFW_PRESS)
+    input.enter = true;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    input.esc = true;
+  if (key == GLFW_KEY_DOWN && action == GLFW_RELEASE)
+    input.down = false;
+  if (key == GLFW_KEY_UP && action == GLFW_RELEASE)
+    input.up = false;
+  if (key == GLFW_KEY_LEFT && action == GLFW_RELEASE)
+    input.left = false;
+  if (key == GLFW_KEY_RIGHT && action == GLFW_RELEASE)
+    input.right = false;
+  if (key == GLFW_KEY_ENTER && action == GLFW_RELEASE)
+    input.enter = false;
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+    input.esc = false;
+  
 }
 
 int main(int argc, char **argv) {
-  // Set up a console, if using MinGW
-  // This is because the mwindows linker flag,
-  // used by blocks referenced by small3d,
-  // eliminates the default one.
-#ifdef __MINGW32__
-  AllocConsole();
-  freopen("CONOUT$", "w", stdout);
-#endif
-
+  
   try {
     initLogger();
     GameLogic gameLogic;
-
+    
     // program main loop
     
     // seconds, for setting the framerate
     double seconds = glfwGetTime();
     double prevSeconds = seconds;
     double secondsInterval = 1.0 / frameRate;
-
+    
     GLFWwindow* window = gameLogic.renderer.getWindow();
-
+    
     glfwSetKeyCallback(window, keyCallback);
-
+    
     while (!glfwWindowShouldClose(window) && !input.esc) {
-
+      
       glfwPollEvents();
-
+      
       seconds = glfwGetTime();
       if (seconds - prevSeconds > secondsInterval) {
-	gameLogic.process(input);
-	prevSeconds = seconds;
-	gameLogic.render();
+        gameLogic.process(input);
+        prevSeconds = seconds;
+        gameLogic.render();
       }
     }
   }
@@ -99,6 +91,6 @@ int main(int argc, char **argv) {
     LOGERROR(e.what());
     return EXIT_FAILURE;
   }
-
+  
   return EXIT_SUCCESS;
 }
